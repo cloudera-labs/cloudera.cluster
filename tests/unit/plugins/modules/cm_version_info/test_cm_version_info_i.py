195cea45cf3cff3d75768d794b727fe1535d9a9d
Webster Mudge <wmudge@cloudera.com>
2023-03-06 17:17:34 -0500

    Cloudera Manager module framework (#62)
    * Create common cm_utils.py for managing CM API interactions, unit and integration test frameworks, and cm_version_info and cm_endpoint_info modules.
    * Clean up error messages and remove unused imports
    * Create ad-hoc/unimplemented API resource module, cm_resource_info
    * Fix JSON decode bug for ApiExceptions
    * Centralize call_api method
    * Add documentation and document fragment for cm_resource
    * Create cm_resource module for ad-hoc CM API endpoint calls
    
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

---
+++ tests/unit/plugins/modules/cm_version_info/test_cm_version_info_i.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2022 Cloudera, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import pprint
+import pytest
+import unittest
+
+from ansible_collections.cloudera.cluster.plugins.modules import cm_version_info
+from ansible_collections.cloudera.cluster.tests.unit.plugins.modules.utils import AnsibleExitJson, AnsibleFailJson, ModuleTestCase, setup_module_args
+
+
+@unittest.skipUnless(os.getenv('CM_USERNAME'), "Cloudera Manager access parameters not set")
+class TestCMVersionIntegration(ModuleTestCase):
+    
+    def test_host_discovery(self):
+        setup_module_args({
+            "username": os.getenv('CM_USERNAME'),
+            "password": os.getenv('CM_PASSWORD'),
+            "host": os.getenv('CM_HOST'),
+            "port": "7180",
+            "verify_tls": "no",
+            "debug": "yes"
+        })
+        
+        with pytest.raises(AnsibleExitJson) as e:
+            cm_version_info.main()
+            
+        self.assertEquals(e.value.args[0]['cm']['version'], "7.6.5")
+        
+    def test_direct_endpoint(self):
+        setup_module_args({
+            "username": os.getenv('CM_USERNAME'),
+            "password": os.getenv('CM_PASSWORD'),
+            "url": "https://" + os.getenv('CM_HOST') + ":" + os.getenv('CM_PORT_TLS') + "/api/" + os.getenv('CM_VERSION'),
+            "verify_tls": "no",
+            "debug": "yes"
+        })
+        
+        with pytest.raises(AnsibleExitJson) as e:
+            cm_version_info.main()
+            
+        self.assertEquals(e.value.args[0]['cm']['version'], "7.6.5")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/unit/plugins/modules/utils.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2022 Cloudera, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import json
+import unittest
+
+from unittest.mock import patch
+from ansible.module_utils import basic
+from ansible.module_utils.common.text.converters import to_bytes
+
+def setup_module_args(args):
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)
+
+
+class AnsibleExitJson(Exception):
+    pass
+
+
+class AnsibleFailJson(Exception):
+    pass
+
+
+def exit_json(*args, **kwargs):
+    if 'changed' not in kwargs:
+        kwargs['changed'] = False
+    raise AnsibleExitJson(kwargs)
+
+def fail_json(*args, **kwargs):
+    kwargs['failed'] = True
+    raise AnsibleFailJson(kwargs)
+
+
+class ModuleTestCase(unittest.TestCase):
+    def setUp(self):
+        self.mock_module = patch.multiple(basic.AnsibleModule, 
+                                          exit_json=exit_json, 
+                                          fail_json=fail_json)
+        self.mock_module.start()
+        self.mock_sleep = patch('time.sleep')
+        self.mock_sleep.start()
+        setup_module_args({})
+        self.addCleanup(self.mock_module.stop)
+        self.addCleanup(self.mock_sleep.stop)
