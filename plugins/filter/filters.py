45b81e9d8b36366bd159ccdc0594f6836195fb94
Webster Mudge <wmudge@cloudera.com>
2023-09-28 19:16:00 -0400

    Update release/v4.0.0 (#132)
    * Add 'freeipa_enroll' optional parameter to suppress client enrollment with FreeIPA server (#129)
    * Update license headers and copyright
    * Update version to v4.0.0
    
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- plugins/filter/filters.py
+++ plugins/filter/filters.py
@@ -1,4 +1,7 @@
-# Copyright 2021 Cloudera, Inc.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2023 Cloudera, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from __future__ import absolute_import, division, print_function
 from typing import Optional
 import re
