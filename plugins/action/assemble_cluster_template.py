c3e5093c34e2a2253e978e0706645f2afc16f642
Webster Mudge <wmudge@cloudera.com>
2023-11-17 12:19:33 -0500

    Update to handle aliases, temp file cleanup, documentation for assemble_cluster_template action plugin
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- plugins/action/assemble_cluster_template.py
+++ plugins/action/assemble_cluster_template.py
@@ -85,7 +85,6 @@ class ActionModule(ActionBase):
                         i for i in base if i[idempotent_id] == item[idempotent_id]
                     ]
                     if namesake:
-                        # LOG.warn("List item being replaced, Old: [%s], New: [%s]", namesake[0], item)
                         self.update_dict(
                             namesake[0],
                             item,
@@ -100,7 +99,7 @@ class ActionModule(ActionBase):
             base.append(item)
         base.sort(key=lambda x: json.dumps(x, sort_keys=True))
 
-    def _assemble_fragments(
+    def assemble_fragments(
         self, assembled_file, src_path, regex=None, ignore_hidden=True, decrypt=True
     ):
         # By file name sort order
@@ -128,14 +127,15 @@ class ActionModule(ActionBase):
                     self.update_object(self.MERGED, json.loads(fragment_file.read()))
                 except json.JSONDecodeError as e:
                     raise AnsibleActionFail(
-                        message=f"{to_text(e.msg)}", obj=to_native(e)
+                        message=f"JSON parsing error: {to_text(e.msg)}",
+                        obj=to_native(e),
                     )
 
         # Write out the final assembly
         json.dump(self.MERGED, assembled_file, indent=2, sort_keys=False)
 
-        # Close the assembled file handle
-        assembled_file.close()
+        # Flush the assembled file handle
+        assembled_file.flush()
 
     def run(self, tmp=None, task_vars=None):
         self._supports_check_mode = False
@@ -146,18 +146,29 @@ class ActionModule(ActionBase):
         if task_vars is None:
             task_vars = dict()
 
+        # Handle aliases
         src = self._task.args.get("src", None)
+        if src is None:
+            src = self._task.args.get("cluster_template_src", None)
+
         dest = self._task.args.get("dest", None)
-        remote_src = self._task.args.get("remote_src", False)
+        if dest is None:
+            dest = self._task.args.get("cluster_template")
+
         regexp = self._task.args.get("regexp", None)
-        follow = self._task.args.get("follow", False)
+        if regexp is None:
+            regexp = self._task.args.get("filter", None)
+
+        remote_src = boolean(self._task.args.get("remote_src", False))
+        follow = boolean(self._task.args.get("follow", False))
         ignore_hidden = boolean(self._task.args.get("ignore_hidden", True))
         decrypt = self._task.args.pop("decrypt", True)
 
         try:
             if src is None or dest is None:
-                raise AnsibleActionFail("src and dest are required")
+                raise AnsibleActionFail("Both 'src' and 'dest' are required")
 
+            # If src files are on the remote host, run the module
             if boolean(remote_src, strict=False):
                 result.update(
                     self._execute_module(
@@ -175,14 +186,16 @@ class ActionModule(ActionBase):
             if not os.path.isdir(src):
                 raise AnsibleActionFail(f"Source, {src}, is not a directory")
 
+            # Compile the regexp
             compiled = None
             if regexp is not None:
                 compiled = re.compile(regexp)
 
+            # Assemble the src files into output file
             with tempfile.NamedTemporaryFile(
-                mode="w", encoding="utf-8", dir=C.DEFAULT_LOCAL_TMP, delete=False
+                mode="w", encoding="utf-8", dir=C.DEFAULT_LOCAL_TMP
             ) as assembled:
-                self._assemble_fragments(
+                self.assemble_fragments(
                     assembled,
                     src,
                     regex=compiled,
@@ -190,54 +203,76 @@ class ActionModule(ActionBase):
                     decrypt=decrypt,
                 )
 
-            assembled_checksum = checksum_s(assembled.name)
-            dest = self._remote_expand_user(dest)
-            dest_stat = self._execute_remote_stat(
-                dest, all_vars=task_vars, follow=follow
-            )
+                # Gather the checksums for assembled file and destination file
+                assembled_checksum = checksum_s(assembled.name)
 
-            new_module_args = self._task.args.copy()
-
-            # Purge cloudera.cluster.assemble_cluster_template-specific options
-            for o in ["remote_src", "regexp", "filter", "ignore_hidden", "decrypt", ]:
-                new_module_args.pop(o, None)
-
-            new_module_args.update(dest=dest)
-
-            diff = {}
-            if assembled_checksum != dest_stat["checksum"]:
-                if self._task.diff:
-                    diff = self._get_diff_data(dest, assembled.name, task_vars)
-
-                remote_path = self._connection._shell.join_path(
-                    self._connection._shell.tmpdir, "assembled_cluster_template"
+                dest = self._remote_expand_user(dest)
+                dest_stat = self._execute_remote_stat(
+                    dest, all_vars=task_vars, follow=follow
                 )
-                transfered = self._transfer_file(assembled.name, remote_path)
 
-                self._fixup_perms2((self._connection._shell.tmpdir, remote_path))
+                # Prepare the task arguments for the called submodules
+                submodule_args = self._task.args.copy()
 
-                new_module_args.update(
-                    dict(
-                        src=transfered,
+                # Purge non-submodule arguments
+                for o in [
+                    "cluster_template_src",
+                    "cluster_template",
+                    "remote_src",
+                    "regexp",
+                    "filter",
+                    "ignore_hidden",
+                    "decrypt",
+                ]:
+                    submodule_args.pop(o, None)
+
+                # Update the 'dest' arg
+                submodule_args.update(dest=dest)
+
+                if assembled_checksum != dest_stat["checksum"]:
+                    diff = {}
+                    
+                    if self._task.diff:
+                        diff = self._get_diff_data(dest, assembled.name, task_vars)
+
+                    # Define a temporary remote path for the remote copy
+                    remote_path = self._connection._shell.join_path(
+                        self._connection._shell.tmpdir, "assembled_cluster_template"
                     )
-                )
+                    
+                    # Transfer the file to the remote path
+                    transfered = self._transfer_file(assembled.name, remote_path)
 
-                copy = self._execute_module(
-                    module_name="ansible.legacy.copy",
-                    module_args=new_module_args,
-                    task_vars=task_vars,
-                )
+                    # Update the file permissions on the remote file
+                    self._fixup_perms2((self._connection._shell.tmpdir, remote_path))
 
-                if diff:
-                    copy.update(diff=diff)
-                result.update(copy)
-            else:
-                file = self._execute_module(
-                    module_name="ansible.legacy.file",
-                    module_args=new_module_args,
-                    task_vars=task_vars,
-                )
-                result.update(file)
+                    # Update the 'src' arg with the temporary remote file
+                    submodule_args.update(
+                        dict(
+                            src=transfered,
+                        )
+                    )
+
+                    # Execute the copy
+                    copy = self._execute_module(
+                        module_name="ansible.legacy.copy",
+                        module_args=submodule_args,
+                        task_vars=task_vars,
+                    )
+
+                    if diff:
+                        copy.update(diff=diff)
+                    
+                    result.update(copy)
+                else:
+                    # Gather details on the existing file
+                    file = self._execute_module(
+                        module_name="ansible.legacy.file",
+                        module_args=submodule_args,
+                        task_vars=task_vars,
+                    )
+                    
+                    result.update(file)
         except AnsibleAction as e:
             result.update(e.result)
         finally:
