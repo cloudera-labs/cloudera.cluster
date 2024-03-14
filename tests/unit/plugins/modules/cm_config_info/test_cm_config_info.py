042416b996762a496d70550dbdb3cde3f38d5c37
Webster Mudge <wmudge@cloudera.com>
2024-03-14 17:06:06 -0400

    Refactor tests for cm_endpoint_info Add basic integration tests for cm_config_info Update pytest.ini for AnsibleCollectionFinder warning
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- tests/unit/plugins/modules/cm_config_info/test_cm_config_info.py
+++ tests/unit/plugins/modules/cm_config_info/test_cm_config_info.py
@@ -14,40 +14,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
+import json
 import logging
+import os
 import pytest
 
-from ansible_collections.cloudera.cluster.plugins.modules import cm_endpoint_info
+from ansible_collections.cloudera.cluster.plugins.modules import cm_config_info
 from ansible_collections.cloudera.cluster.tests.unit import AnsibleExitJson, AnsibleFailJson
 
-from cm_client.rest import RESTClientObject
-from urllib3.response import HTTPResponse
-
 LOG = logging.getLogger(__name__)
-    
-def test_host_discovery(module_args, monkeypatch):
-    spec = {
-        "username": "testuser",
-        "password": "testpassword",
-        "host": "test.cldr.info",
+
+
+@pytest.fixture()
+def conn():
+    return {
+        "username": os.getenv('CM_USERNAME'),
+        "password": os.getenv('CM_PASSWORD'),
+        "host": os.getenv('CM_HOST'),
         "port": "7180",
         "verify_tls": "no",
-        "debug": "yes"
+        "debug": "yes",
     }
-    
-    def response():
-        return HTTPResponse()
-    
-    monkeypatch.setattr("urllib3.HTTPConnectionPool.urlopen", response)
-    
-    
-    module_args(spec)
-    
+
+def test_get_cluster_config(conn, module_args):
+    module_args(conn)
+
     with pytest.raises(AnsibleExitJson) as e:
-        cm_endpoint_info.main()
-        
-    assert e.value.endpoint == f"https://{spec['host']}:7183/api/v01"
-    
+        cm_config_info.main()
+
+    assert len(e.value.config) > 0
+
+def test_get_cluster_config_full(conn, module_args):
+    module_args({
+        **conn,
+        "view": "full"
+    })
+
+    with pytest.raises(AnsibleExitJson) as e:
+        cm_config_info.main()
+
+    assert len(e.value.config) > 0
