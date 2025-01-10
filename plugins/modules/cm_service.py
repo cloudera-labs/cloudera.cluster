68ce8613ba1e5b15ff9c93e33a7d8671e816b529
Webster Mudge <wmudge@cloudera.com>
2025-01-10 16:45:29 -0500

    Add diff_mode and platform documentation attributes
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- plugins/modules/cm_service.py
+++ plugins/modules/cm_service.py
@@ -127,6 +127,10 @@ extends_documentation_fragment:
 attributes:
   check_mode:
     support: full
+  diff_mode:
+    support: full
+  platform:
+    platforms: all
 requirements:
   - cm-client
 seealso:
