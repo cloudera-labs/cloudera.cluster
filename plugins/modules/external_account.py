28ef0815d067093a37be0909f1147599f524bf25
rsuplina <rsuplina@cloudera.com>
2025-03-04 15:04:01 +0000

    Add External Accout module
    Signed-off-by: rsuplina <rsuplina@cloudera.com>

---
+++ plugins/modules/external_account.py
@@ -0,0 +1,383 @@
+# Copyright 2024 Cloudera, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ansible_collections.cloudera.cluster.plugins.module_utils.cm_utils import (
+    ClouderaManagerModule,
+)
+from cm_client.rest import ApiException
+from cm_client import (
+    ExternalAccountsResourceApi,
+    ApiExternalAccount,
+    ApiConfig,
+    ApiConfigList,
+)
+import json
+
+ANSIBLE_METADATA = {
+    "metadata_version": "1.1",
+    "status": ["preview"],
+    "supported_by": "community",
+}
+
+DOCUMENTATION = r"""
+---
+module: external_account
+short_description: Create, update, or delete an external module account
+description:
+  - Manage external accounts, including creation, updates, and deletion.
+  - Supports a variety of account types such as AWS, Azure, Altus, and Basic Authentication.
+  - Configure account-specific parameters, including access keys, client secrets, or basic credentials.
+  - Supports I(check_mode).
+author:
+  - "Ronald Suplina (@rsuplina)"
+options:
+  name:
+    description:
+      - The initial name of the account.
+    type: str
+    required: no
+  category:
+    description:
+      - The category of the account.
+    type: str
+    required: no
+    choices:
+      - AWS
+      - ALTUS
+      - AZURE
+      - BASICAUTH
+  state:
+    description:
+      - If I(state=present), the account will be created or updated.
+      - If I(state=absent), the account will be deleted.
+    type: str
+    required: no
+    default: present
+    choices:
+      - present
+      - absent
+  type:
+    description:
+      - The type of the external account.
+    type: str
+    required: no
+    choices:
+      - AWS_ACCESS_KEY_AUTH
+      - AWS_IAM_ROLES_AUTH
+      - ALTUS_ACCESS_KEY_AUTH
+      - ADLS_AD_SVC_PRINC_AUTH
+      - BASIC_AUTH
+  params:
+    description:
+      - A dictionary of parameters for the external account configuration.
+      - The required parameters depend on the type of the account.
+    type: dict
+    required: no
+    suboptions:
+      aws_access_key:
+        description:
+          - The AWS access key for AWS_ACCESS_KEY_AUTH.
+        type: str
+      aws_secret_key:
+        description:
+          - The AWS secret key for AWS_ACCESS_KEY_AUTH.
+        type: str
+      access_key_id:
+        description:
+          - The Altus access key ID for ALTUS_ACCESS_KEY_AUTH.
+        type: str
+      private_key:
+        description:
+          - The private key for ALTUS_ACCESS_KEY_AUTH.
+        type: str
+      adls_tenant_id:
+        description:
+          - The Azure AD tenant ID for ADLS_AD_SVC_PRINC_AUTH.
+        type: str
+      adls_client_id:
+        description:
+          - The Azure AD client ID for ADLS_AD_SVC_PRINC_AUTH.
+        type: str
+      adls_client_key:
+        description:
+          - The Azure AD client secret key for ADLS_AD_SVC_PRINC_AUTH.
+        type: str
+      username:
+        description:
+          - The username for BASIC_AUTH.
+        type: str
+      password:
+        description:
+          - The password for BASIC_AUTH.
+        type: str
+attributes:
+  check_mode:
+    support: full
+"""
+EXAMPLES = r"""
+---
+- name: Create AWS Access key credentials
+  cloudera.cluster.external_account:
+    host: example.cloudera.com
+    port: "7180"
+    username: "jane_smith"
+    password: "S&peR4Ec*re"
+    name: access_key_1
+    state: present
+    type: AWS
+    category: AWS_ACCESS_KEY_AUTH
+    params:
+      aws_access_key: access_key1
+      aws_secret_key: secret_key1
+
+- name: Create basic authentication credentials
+  cloudera.cluster.external_account:
+    host: example.cloudera.com
+    port: "7180"
+    username: "jane_smith"
+    password: "S&peR4Ec*re"
+    name: Jane
+    state: present
+    type: BASIC_AUTH
+    category: BASICAUTH
+    params:
+      username: jane_user
+      password: pass123!
+
+- name: Update AWS Access key credentials
+  cloudera.cluster.external_account:
+    host: example.cloudera.com
+    port: "7180"
+    username: "jane_smith"
+    password: "S&peR4Ec*re"
+    name: access_key_1
+    state: present
+    type: AWS
+    category: AWS_ACCESS_KEY_AUTH
+    params:
+      aws_access_key: access_key2
+      aws_secret_key: secret_key2
+
+- name: Remove basic authentication credentials
+  cloudera.cluster.external_account:
+    host: example.cloudera.com
+    port: "7180"
+    username: "jane_smith"
+    password: "S&peR4Ec*re"
+    name: Jane
+    state: absent
+
+"""
+
+RETURN = r"""
+---
+external_account:
+    description: Details of the external account created, updated, or retrieved.
+    type: dict
+    elements: complex
+    returned: always
+    contains:
+        name:
+            description: Represents the initial name of the account.
+            type: str
+            returned: always
+        displayName:
+            description: A modifiable label to identify this account for user-visible purposes.
+            type: str
+            returned: always
+        createdTime:
+            description: The time of creation for this account.
+            type: str
+            returned: always
+        lastModifiedTime:
+            description: The last modification time for this account.
+            type: str
+            returned: always
+        typeName:
+            description: The Type ID of a supported external account type.
+            type: str
+            returned: always
+        accountConfigs:
+            description: The configuration options for this account.
+            type: list
+            elements: dict
+            returned: always
+"""
+
+
+class ClouderaExternalAccount(ClouderaManagerModule):
+    def __init__(self, module):
+        super(ClouderaExternalAccount, self).__init__(module)
+
+        # Set the parameters
+        self.name = self.get_param("name")
+        self.category = self.get_param("category")
+        self.type = self.get_param("type")
+        self.params = self.get_param("params")
+        self.state = self.get_param("state")
+
+        # Initialize the return values
+        self.external_account_output = []
+        self.changed = False
+        # Execute the logic
+        self.process()
+
+
+    @ClouderaManagerModule.handle_process
+    def process(self):
+        api_instance = ExternalAccountsResourceApi(self.api_client)
+        existing = []
+        self.params = {
+            key: value for key, value in self.params.items() if value is not None
+        }
+        try:
+            existing = api_instance.read_account(self.name).to_dict()
+        except ApiException as ex:
+            if ex.status == 400:
+                pass
+            else:
+                raise ex
+
+        if self.state == "present":
+            try:
+                if existing:
+                    if not self.module.check_mode:
+                        self.external_account_output = api_instance.update_account(
+                            body=ApiExternalAccount(
+                                name=self.name,
+                                display_name=self.name,
+                                type_name=self.type,
+                                account_configs=ApiConfigList(
+                                    items=[
+                                        ApiConfig(name=key, value=value)
+                                        for key, value in self.params.items()
+                                    ]
+                                ),
+                            )
+                        )
+                        self.changed = True
+                else:
+                    if not self.module.check_mode:
+                        self.external_account_output = api_instance.create_account(
+                            body=ApiExternalAccount(
+                                name=self.name,
+                                display_name=self.name,
+                                type_name=self.type,
+                                account_configs=ApiConfigList(
+                                    items=[
+                                        ApiConfig(name=key, value=value)
+                                        for key, value in self.params.items()
+                                    ]
+                                ),
+                            )
+                        )
+                        self.changed = True
+
+            except ApiException as ex:
+                if ex.status == 400:
+                    self.module.fail_json(msg=json.loads(ex.body)["message"])
+                else:
+                    raise ex
+
+        if self.state == "absent":
+            if existing:
+                self.external_account_output = api_instance.delete_account(self.name)
+                self.changed = True
+
+
+def main():
+
+    module = ClouderaManagerModule.ansible_module(
+        argument_spec=dict(
+            name=dict(required=False, type="str"),
+            category=dict(
+                type="str",
+                required=False,
+                choices=["AWS", "ALTUS", "AZURE", "BASICAUTH"],
+            ),
+            state=dict(
+                type="str",
+                default="present",
+                choices=["present", "absent"],
+            ),
+            type=dict(
+                type="str",
+                required=False,
+                choices=[
+                    "AWS_ACCESS_KEY_AUTH",
+                    "AWS_IAM_ROLES_AUTH",
+                    "ALTUS_ACCESS_KEY_AUTH",
+                    "ADLS_AD_SVC_PRINC_AUTH",
+                    "BASIC_AUTH",
+                ],
+            ),
+            params=dict(
+                type="dict",
+                default={},
+                options=dict(
+                    # AWS params
+                    aws_access_key=dict(type="str"),
+                    aws_secret_key=dict(type="str"),
+                    # ALTUS params
+                    access_key_id=dict(type="str"),
+                    private_key=dict(type="str"),
+                    # AZURE params
+                    adls_tenant_id=dict(type="str"),
+                    adls_client_id=dict(type="str"),
+                    adls_client_key=dict(type="str"),
+                    # BASIC auth params
+                    username=dict(type="str"),
+                    password=dict(type="str"),
+                ),
+            ),
+            required_if={
+                "AWS_ACCESS_KEY_AUTH": [
+                    "params.aws_access_key",
+                    "params.aws_secret_key",
+                ],
+                "ADLS_AD_SVC_PRINC_AUTH": [
+                    "params.adls_client_id",
+                    "params.adls_client_id",
+                    "params.adls_tenant_id",
+                ],
+                "ALTUS_ACCESS_KEY_AUTH": [
+                    "params.access_key_id",
+                    "params.private_key",
+                ],
+                "BASIC_AUTH": [
+                    "params.username",
+                    "params.password",
+                ],
+            },
+        ),
+        supports_check_mode=True,
+    )
+
+    result = ClouderaExternalAccount(module)
+
+    output = dict(
+        changed=result.changed,
+        external_account_output=result.external_account_output,
+    )
+
+    if result.debug:
+        log = result.log_capture.getvalue()
+        output.update(debug=log, debug_lines=log.split("\n"))
+
+    module.exit_json(**output)
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ plugins/modules/external_account_info.py
@@ -0,0 +1,211 @@
+# Copyright 2024 Cloudera, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ansible_collections.cloudera.cluster.plugins.module_utils.cm_utils import (
+    ClouderaManagerModule,
+)
+from cm_client.rest import ApiException
+from cm_client import (
+    ExternalAccountsResourceApi,
+)
+
+ANSIBLE_METADATA = {
+    "metadata_version": "1.1",
+    "status": ["preview"],
+    "supported_by": "community",
+}
+
+DOCUMENTATION = r"""
+---
+module: external_account_info
+short_description: Retrieve external account details details.
+description:
+  - Provides details for a specific account or retrieves all external accounts configured in Cloudera Manager.
+author:
+  - "Ronald Suplina (@rsuplina)"
+requirements:
+  - cm_client
+options:
+  name:
+    description:
+      - The initial name of the account.
+    type: str
+    required: no
+  type:
+    description:
+      - The type of the external account.
+    type: str
+    required: no
+    choices:
+      - AWS_ACCESS_KEY_AUTH
+      - AWS_IAM_ROLES_AUTH
+      - ALTUS_ACCESS_KEY_AUTH
+      - ADLS_AD_SVC_PRINC_AUTH
+      - BASIC_AUTH
+"""
+
+EXAMPLES = r"""
+---
+- name: Get all external accounts in Cloudera Manager
+  cloudera.cluster.external_account_info:
+    host: example.cloudera.com
+    port: "7180"
+    username: "jane_smith"
+    password: "S&peR4Ec*re"
+
+- name: Get all external accounts with type "AWS_ACCESS_KEY_AUTH"
+  cloudera.cluster.external_account_info:
+    host: example.cloudera.com
+    port: "7180"
+    username: "jane_smith"
+    password: "S&peR4Ec*re"
+    type: "AWS_ACCESS_KEY_AUTH"
+
+- name: Get specific external account
+  cloudera.cluster.external_account_info:
+    host: example.cloudera.com
+    port: "7180"
+    username: "jane_smith"
+    password: "S&peR4Ec*re"
+    name: "aws_test_key"
+
+"""
+
+RETURN = r"""
+---
+external_account:
+    description:
+        - List of one or more external accounts within the cluster.
+    type: list
+    elements: dict
+    returned: always
+    contains:
+        name:
+            description: Represents the initial name of the account.
+            type: str
+            returned: always
+        displayName:
+            description: A modifiable label to identify this account for user-visible purposes.
+            type: str
+            returned: always
+        createdTime:
+            description: The time of creation for this account.
+            type: str
+            returned: always
+        lastModifiedTime:
+            description: The last modification time for this account.
+            type: str
+            returned: always
+        typeName:
+            description: The Type ID of a supported external account type.
+            type: str
+            returned: always
+        accountConfigs:
+            description: The configuration options for this account.
+            type: list
+            elements: dict
+            returned: always
+"""
+
+
+class ClouderaExternalAccountInfo(ClouderaManagerModule):
+    def __init__(self, module):
+        super(ClouderaExternalAccountInfo, self).__init__(module)
+
+        # Set the parameters
+        self.name = self.get_param("name")
+        self.type = self.get_param("type")
+
+        # Initialize the return values
+        self.external_account_info_output = []
+        self.changed = False
+
+        # Execute the logic
+        self.process()
+
+    @ClouderaManagerModule.handle_process
+    def process(self):
+        api_instance = ExternalAccountsResourceApi(self.api_client)
+        account_types = [
+            "AWS_ACCESS_KEY_AUTH",
+            "AWS_IAM_ROLES_AUTH",
+            "ALTUS_ACCESS_KEY_AUTH",
+            "ADLS_AD_SVC_PRINC_AUTH",
+            "BASIC_AUTH",
+        ]
+        try:
+            if self.name:
+                self.external_account_info_output = [
+                    api_instance.read_account(self.name).to_dict()
+                ]
+
+            elif self.type:
+                self.external_account_info_output = api_instance.read_accounts(self.type).to_dict().get("items", [])
+
+            else:
+
+                self.external_account_info_output = api_instance.read_accounts(
+                    type_name="AWS_ACCESS_KEY_AUTH"
+                ).to_dict()["items"]
+                all_accounts = []
+                for account_type in account_types:
+                    accounts = (
+                        api_instance.read_accounts(type_name=account_type)
+                        .to_dict()
+                        .get("items", [])
+                    )
+                    all_accounts.extend(accounts)
+                self.external_account_info_output = all_accounts
+
+        except ApiException as e:
+            if e.status == 404:
+                self.cm_cluster_info = f"Account {self.name} does not exist."
+                self.module.fail_json(msg=str(self.external_account_info_output))
+
+
+def main():
+    module = ClouderaManagerModule.ansible_module(
+        argument_spec=dict(
+            name=dict(required=False, type="str"),
+            type=dict(
+                type="str",
+                required=False,
+                choices=[
+                    "AWS_ACCESS_KEY_AUTH",
+                    "AWS_IAM_ROLES_AUTH",
+                    "ALTUS_ACCESS_KEY_AUTH",
+                    "ADLS_AD_SVC_PRINC_AUTH",
+                    "BASIC_AUTH",
+                ],
+            ),
+        ),
+        supports_check_mode=False,
+    )
+
+    result = ClouderaExternalAccountInfo(module)
+
+    output = dict(
+        changed=False,
+        external_account_info_output=result.external_account_info_output,
+    )
+
+    if result.debug:
+        log = result.log_capture.getvalue()
+        output.update(debug=log, debug_lines=log.split("\n"))
+
+    module.exit_json(**output)
+
+
+if __name__ == "__main__":
+    main()
