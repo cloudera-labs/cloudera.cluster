d98ce455cb2ecdf93e5f0d6b78fd7c3ca4e06d74
Webster Mudge <wmudge@cloudera.com>
2025-03-07 11:20:20 -0500

    Remove extra line
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- .test.sh
+++ .test.sh
@@ -20,4 +20,3 @@
 mkdir -p .ansible/collections/ansible_collections/cloudera/cluster
 git archive $(git rev-parse --abbrev-ref HEAD) | tar -x -C .ansible/collections/ansible_collections/cloudera/cluster
 (cd .ansible/collections/ansible_collections/cloudera/cluster && pytest)
-
