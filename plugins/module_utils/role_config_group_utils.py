e0c2bd924eb6ef79e06a92b1dbff9b0759248081
Webster Mudge <wmudge@cloudera.com>
2025-04-07 15:02:23 -0400

    Add utilities for creating models, provisioning, and updating role config groups Update the get_base_role_config_group to retrieve base groups for all types
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- plugins/module_utils/role_config_group_utils.py
+++ plugins/module_utils/role_config_group_utils.py
@@ -14,12 +14,20 @@
 
 from ansible_collections.cloudera.cluster.plugins.module_utils.cm_utils import (
     normalize_output,
+    ConfigListUpdates,
+)
+from ansible_collections.cloudera.cluster.plugins.module_utils.role_utils import (
+    InvalidRoleTypeException,
 )
 
 from cm_client import (
     ApiClient,
+    ApiConfig,
+    ApiConfigList,
     ApiRoleConfigGroup,
+    ApiRoleConfigGroupList,
     RoleConfigGroupsResourceApi,
+    ServicesResourceApi,
     MgmtRoleConfigGroupsResourceApi,
 )
 
@@ -55,22 +63,104 @@ def parse_role_config_group_result(role_config_group: ApiRoleConfigGroup) -> dic
     return output
 
 
-def get_base_role_config_group(
-    api_client: ApiClient, cluster_name: str, service_name: str, role_type: str
+def create_role_config_group(
+    api_client: ApiClient,
+    cluster_name: str,
+    service_name: str,
+    name: str,
+    role_type: str,
+    display_name: str = None,
+    config: dict = None,
 ) -> ApiRoleConfigGroup:
-    rcg_api = RoleConfigGroupsResourceApi(api_client)
-    return next(
-        iter(
-            [
-                r
-                for r in rcg_api.read_role_config_groups(
-                    cluster_name, service_name
-                ).items
-                if r.role_type == role_type and r.base
-            ]
-        ),
-        None,
+    if (
+        role_type.upper()
+        not in ServicesResourceApi(api_client)
+        .list_role_types(
+            cluster_name=cluster_name,
+            service_name=service_name,
+        )
+        .items
+    ):
+        raise InvalidRoleTypeException(
+            f"Invalid role type '{role_type}' for service '{service_name}'"
+        )
+
+    role_config_group = ApiRoleConfigGroup(
+        name=name,
+        role_type=role_type.upper(),
     )
+
+    if display_name:
+        role_config_group.display_name = display_name
+
+    if config:
+        role_config_group.config = ApiConfigList(
+            items=[ApiConfig(name=k, value=v) for k, v in config.items()]
+        )
+
+    return role_config_group
+
+
+def provision_role_config_groups(
+    api_client: ApiClient,
+    cluster_name: str,
+    service_name: str,
+    role_config_groups: list[ApiRoleConfigGroup],
+) -> ApiRoleConfigGroup:
+    return RoleConfigGroupsResourceApi(api_client).create_role_config_groups(
+        cluster_name=cluster_name,
+        service_name=service_name,
+        body=ApiRoleConfigGroupList(items=role_config_groups),
+    )
+
+
+def update_role_config_group(
+    role_config_group: ApiRoleConfigGroup,
+    display_name: str = None,
+    config: dict = None,
+    purge: bool = False,
+) -> tuple[ApiRoleConfigGroup, dict, dict]:
+    before, after = dict(), dict()
+
+    # Check for display name changes
+    if display_name is not None and display_name != role_config_group.display_name:
+        before.update(display_name=role_config_group.display_name)
+        after.update(display_name=display_name)
+        role_config_group.display_name = display_name
+
+    # Reconcile configurations
+    if config or purge:
+        if config is None:
+            config = dict()
+
+        updates = ConfigListUpdates(role_config_group.config, config, purge)
+
+        if updates.changed:
+            before.update(config=updates.diff["before"])
+            after.update(config=updates.diff["after"])
+            role_config_group.config = updates.config
+
+    return (role_config_group, before, after)
+
+
+# TODO Normalize the return value to be a list
+def get_base_role_config_group(
+    api_client: ApiClient, cluster_name: str, service_name: str, role_type: str = None
+) -> ApiRoleConfigGroup:
+    base_rcg_list = [
+        r
+        for r in RoleConfigGroupsResourceApi(api_client)
+        .read_role_config_groups(
+            cluster_name=cluster_name,
+            service_name=service_name,
+        )
+        .items
+        if (r.base and role_type is None) or (r.base and r.role_type == role_type)
+    ]
+    if role_type is not None:
+        return next(iter(base_rcg_list), None)
+    else:
+        return base_rcg_list
 
 
 def get_mgmt_base_role_config_group(
