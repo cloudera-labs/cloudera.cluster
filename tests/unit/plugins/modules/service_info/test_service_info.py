67ca892735267463f7f7827400fe3b21de98db89
Webster Mudge <wmudge@cloudera.com>
2025-04-17 13:16:12 -0400

    Update service_info module to align with utilities and output Update service_info tests to use pytest fixtures
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- tests/unit/plugins/modules/service_info/test_service_info.py
+++ tests/unit/plugins/modules/service_info/test_service_info.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2024 Cloudera, Inc. All Rights Reserved.
+# Copyright 2025 Cloudera, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,74 +22,127 @@ import logging
 import os
 import pytest
 
+from pathlib import Path
+
+from cm_client import (
+    ApiConfig,
+    ApiEntityTag,
+    ApiHost,
+    ApiHostRef,
+    ApiRole,
+    ApiService,
+    ApiServiceConfig,
+    ApiServiceState,
+    ServicesResourceApi,
+)
+
 from ansible_collections.cloudera.cluster.plugins.modules import service_info
+from ansible_collections.cloudera.cluster.plugins.module_utils.cluster_utils import (
+    get_cluster_hosts,
+)
+from ansible_collections.cloudera.cluster.plugins.module_utils.service_utils import (
+    get_service_hosts,
+)
 from ansible_collections.cloudera.cluster.tests.unit import (
     AnsibleExitJson,
     AnsibleFailJson,
+    deregister_service,
+    register_service,
 )
 
 LOG = logging.getLogger(__name__)
 
 
+@pytest.fixture(scope="module")
+def zookeeper(cm_api_client, base_cluster, request):
+    # Keep track of the provisioned service(s)
+    service_registry = list[ApiService]()
+
+    # Get the current cluster hosts
+    hosts = get_cluster_hosts(cm_api_client, base_cluster)
+
+    id = Path(request.node.name).stem
+
+    zk_service = ApiService(
+        name=f"test-zk-{id}",
+        type="ZOOKEEPER",
+        display_name=f"ZooKeeper ({id})",
+        # Add a SERVER role (so we can start the service -- a ZK requirement!)
+        roles=[ApiRole(type="SERVER", host_ref=ApiHostRef(hosts[0].host_id))],
+    )
+
+    # Provision and yield the created service
+    yield register_service(
+        api_client=cm_api_client,
+        registry=service_registry,
+        cluster=base_cluster,
+        service=zk_service,
+    )
+
+    # Remove the created service
+    deregister_service(api_client=cm_api_client, registry=service_registry)
+
+
 @pytest.fixture()
-def conn():
-    conn = dict(username=os.getenv("CM_USERNAME"), password=os.getenv("CM_PASSWORD"))
+def cluster_hosts(cm_api_client, base_cluster) -> list[ApiHost]:
+    return get_cluster_hosts(cm_api_client, base_cluster)
 
-    if os.getenv("CM_HOST", None):
-        conn.update(host=os.getenv("CM_HOST"))
 
-    if os.getenv("CM_PORT", None):
-        conn.update(port=os.getenv("CM_PORT"))
+@pytest.fixture()
+def available_hosts(cm_api_client, cluster_hosts, zookeeper) -> list[ApiHost]:
+    service_host_ids = [
+        h.host_id
+        for h in get_service_hosts(
+            api_client=cm_api_client,
+            service=zookeeper,
+        )
+    ]
 
-    if os.getenv("CM_ENDPOINT", None):
-        conn.update(url=os.getenv("CM_ENDPOINT"))
-
-    if os.getenv("CM_PROXY", None):
-        conn.update(proxy=os.getenv("CM_PROXY"))
-
-    return {
-        **conn,
-        "verify_tls": "no",
-        "debug": "no",
-    }
+    return [h for h in cluster_hosts if h.host_id not in service_host_ids]
 
 
 def test_missing_required(conn, module_args):
-    module_args(conn)
+    module_args(
+        {
+            **conn,
+        }
+    )
 
     with pytest.raises(AnsibleFailJson, match="cluster"):
         service_info.main()
 
 
 def test_missing_cluster(conn, module_args):
-    conn.update(service="example")
-    module_args(conn)
-
-    with pytest.raises(AnsibleFailJson, match="cluster"):
-        service_info.main()
-
-
-def test_invalid_service(conn, module_args):
     module_args(
         {
             **conn,
-            "cluster": os.getenv("CM_CLUSTER"),
-            "service": "BOOM",
+            "service": "example",
         }
     )
 
-    with pytest.raises(AnsibleExitJson) as e:
+    with pytest.raises(AnsibleFailJson, match="cluster"):
         service_info.main()
-
-    assert len(e.value.services) == 0
 
 
 def test_invalid_cluster(conn, module_args):
     module_args(
         {
             **conn,
-            "cluster": "BOOM",
-            "service": os.getenv("CM_SERVICE"),
+            "cluster": "invalid",
+            "service": "example",
+        }
+    )
+
+    with pytest.raises(AnsibleFailJson, match="Cluster does not exist") as e:
+        service_info.main()
+
+
+def test_invalid_service(conn, module_args, base_cluster):
+    module_args(
+        {
+            **conn,
+            "cluster": base_cluster.name,
+            "service": "not_found",
         }
     )
 
@@ -99,30 +152,59 @@ def test_invalid_cluster(conn, module_args):
     assert len(e.value.services) == 0
 
 
-def test_view_all_services(conn, module_args):
+def test_all_services(
+    conn,
+    module_args,
+    request,
+    base_cluster,
+    zookeeper,
+    available_hosts,
+    service_factory,
+):
+    id = Path(request.node.name)
+
+    # Add an additional ZooKeeper service
+    zookeeper_two = service_factory(
+        cluster=base_cluster,
+        service=ApiService(
+            name=f"test-zk-{id}",
+            type="ZOOKEEPER",
+            display_name=f"ZooKeeper ({id})",
+            # Add a SERVER role (so we can start the service -- a ZK requirement!)
+            roles=[
+                ApiRole(type="SERVER", host_ref=ApiHostRef(available_hosts[0].host_id))
+            ],
+        ),
+    )
+
     module_args(
         {
             **conn,
-            "cluster": os.getenv("CM_CLUSTER"),
+            "cluster": zookeeper.cluster_ref.cluster_name,
         }
     )
 
     with pytest.raises(AnsibleExitJson) as e:
         service_info.main()
 
-    assert len(e.value.services) > 0
+    assert len(e.value.services) == 3  # 2 ZK and 1 core settings
+    service_names = [s["name"] for s in e.value.services]
+    assert zookeeper.name in service_names
+    assert zookeeper_two.name in service_names
 
 
-def test_view_single_service(conn, module_args):
+def test_named_service(conn, module_args, zookeeper):
     module_args(
         {
             **conn,
-            "cluster": os.getenv("CM_CLUSTER"),
-            "service": os.getenv("CM_SERVICE"),
+            "cluster": zookeeper.cluster_ref.cluster_name,
+            "service": zookeeper.name,
         }
     )
 
     with pytest.raises(AnsibleExitJson) as e:
         service_info.main()
 
-    assert len(e.value.services) == 1
+    assert len(e.value.services) == 1  # Single named ZK
+    service_names = [s["name"] for s in e.value.services]
+    assert zookeeper.name in service_names
