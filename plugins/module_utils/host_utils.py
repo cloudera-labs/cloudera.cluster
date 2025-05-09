7a0927c72e51d870898c6737c16ca59b432526d3
Webster Mudge <wmudge@cloudera.com>
2025-05-09 08:55:47 -0400

    Add parse_host_result() and create_host_model() utility functions
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- plugins/module_utils/host_utils.py
+++ plugins/module_utils/host_utils.py
@@ -18,11 +18,67 @@ A common functions for Cloudera Manager hosts
 
 from cm_client import (
     ApiClient,
+    ApiConfig,
+    ApiConfigList,
+    ApiEntityTag,
     ApiHost,
     ApiHostRef,
     HostsResourceApi,
 )
 from cm_client.rest import ApiException
+
+from ansible_collections.cloudera.cluster.plugins.module_utils.cm_utils import (
+    normalize_output,
+)
+
+
+HOST_OUTPUT = [
+    "host_id",
+    "ip_address",
+    "hostname",
+    "rack_id",
+    "last_heartbeat",
+    # 'role_refs': 'list[ApiRoleRef]',
+    "health_summary",
+    "health_checks",
+    #'host_url': 'str',
+    "maintenance_mode",
+    "commission_state",
+    "maintenance_owners",
+    #'config': 'ApiConfigList',
+    "num_cores",
+    "num_physical_cores",
+    "total_phys_mem_bytes",
+    #'entity_status': 'ApiEntityStatus',
+    #'cluster_ref': 'ApiClusterRef',
+    "distribution",
+    "tags",
+]
+
+
+def parse_host_result(host: ApiHost) -> dict:
+    output = dict()
+
+    # Retrieve only the cluster_name if it exists
+    if host.cluster_ref is not None:
+        output.update(cluster_name=host.cluster_ref.cluster_name)
+    else:
+        output.update(cluster_name=None)
+
+    # Parse the host itself
+    output.update(normalize_output(host.to_dict(), HOST_OUTPUT))
+
+    # Parse the host configurations
+    if host.config is not None:
+        output.update(config={c.name: c.value for c in host.config.items})
+
+    # Parse the role names (only the names)
+    if host.role_refs is not None:
+        output.update(
+            roles=[r.role_name for r in host.role_refs],
+        )
+
+    return output
 
 
 def get_host(
@@ -82,3 +138,34 @@ def get_host_ref(
         return ApiHostRef(host.host_id, host.hostname)
     else:
         return None
+
+
+def create_host_model(
+    api_client: ApiClient,
+    hostname: str,
+    ip_address: str,  # TODO Check!
+    rack_id: str = None,
+    config: dict = None,
+    # host_template: str = None, # TODO Check!
+    # roles: list[ApiRole] = None, # TODO Check!
+    # role_config_groups: list[ApiRoleConfigGroup] = None, # TODO Check!
+    tags: dict = None,
+) -> ApiHost:
+    # Set up the hostname and IP address
+    host = ApiHost(hostname=hostname, ip_address=ip_address)
+
+    # Rack ID
+    if rack_id:
+        host.rack_id = rack_id
+
+    # Configuration
+    if config:
+        host.config = ApiConfigList(
+            items=[ApiConfig(name=k, value=v) for k, v in config.items()]
+        )
+
+    # Tags
+    if tags:
+        host.tags = [ApiEntityTag(k, v) for k, v in tags.items()]
+
+    return host
