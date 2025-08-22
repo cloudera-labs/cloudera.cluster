0e29b2386c60b8a2afd1a42fa22f0d7a0e086fcc
Webster Mudge <wmudge@cloudera.com>
2025-08-22 15:05:04 -0400

    Update copyright date (#323)
    * Update copyright date
    * Add missing copyright headers
    * Update Python shebang (remove on filters, add to service_type_info module)
    
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- plugins/modules/service_type_info.py
+++ plugins/modules/service_type_info.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# # Copyright 2024 Cloudera, Inc.
+# # Copyright 2025 Cloudera, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
